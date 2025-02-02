e8f04baaaa8742e77552efe044c2ed0abec175cd
Andrew Le <a.asterisk.le@gmail.com>
2025-02-02 23:48:09 +0000

    Finished Requirement #1 User Registration.

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -38,12 +38,12 @@ public class SocialMediaController {
         // If conditional that check if the Username is blank, or if the password is less that 4 characters, the minimum.
         // Returns status code 400, headers, and account in the body
         if (account.getUsername().isBlank() || account.getPassword().length() < 4){
-            return ResponseEntity.status(400).header("content-type", "application/zip").body(account);
+            return ResponseEntity.status(400).body(null);
         }
         // If conditional that checks through AccountService if account username already exists, if yes
         // return status code 409, headers, and account in the body
         if (accountService.getAccountUsernameExists(account)){
-            return ResponseEntity.status(409).header("content-type", "application/zip").body(account);
+            return ResponseEntity.status(409).body(null);
         }
         // try-catch block to try to register a new account given the information.
         // if a newAccount is not null i.e. a non-null value was returned, send newAccoutn info in ResponseEntity body with status 200
@@ -51,14 +51,15 @@ public class SocialMediaController {
             System.out.print("WE GOT HERE IN TRY BLOCK");
             Account newAccount = accountService.registerAccount(account);
             if (newAccount != null){
-                return ResponseEntity.status(200).header("content-type", "application/zip").body(newAccount);
+                return ResponseEntity.status(200).body(newAccount);
             }
         }
         catch(Exception e){
             e.printStackTrace();
-            return ResponseEntity.status(401).header("content-type", "application/zip").body(account);
         }
 
-        return null;
+        return ResponseEntity.status(500).body(null);
     }
+
+
 }
